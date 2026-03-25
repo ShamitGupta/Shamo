f8b4050aaefb374361d7afd8479ce2d991155365
ShamitGupta <e1459301@u.nus.edu>
2026-03-25 17:19:10 +0800

    Added Sidebar buttons

--- Chatbot/src/Sidebar/Sidebar.jsx
+++ Chatbot/src/Sidebar/Sidebar.jsx
@@ -3,11 +3,16 @@ function Sidebar(){
     return(
         <div className = {styles.Body}>
             <div className = {styles.Sidebar}>
-                Hello
+                <div className = {styles.SidebarButtonsContainer}>
+                    <button className = {styles.SidebarButtons}>New Chat</button>
+                    <button className = {styles.SidebarButtons}>About Us</button>
+                    <button className = {styles.SidebarButtons}>Report an Issue</button>
+                    <button className = {styles.SidebarButtons}>Contact Us</button>
+                </div>
             </div>
 
             <div className = {styles.ChatSection}>
-
+            
             </div>
         </div>
         
