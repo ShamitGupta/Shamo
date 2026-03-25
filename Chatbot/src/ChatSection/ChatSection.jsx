44536d2e7a0cf8b6532418cb38b6c67f888e443d
ShamitGupta <e1459301@u.nus.edu>
2026-03-25 18:09:19 +0800

    Seperated into Components

---
+++ Chatbot/src/ChatSection/ChatSection.jsx
@@ -0,0 +1,13 @@
+import styles from './ChatSection.module.css'
+
+function ChatSection(){
+    return(
+        <div className = {styles.Body}>
+            <div className = {styles.ChatSection}>
+
+            </div>
+        </div>
+    )
+}
+
+export default ChatSection
