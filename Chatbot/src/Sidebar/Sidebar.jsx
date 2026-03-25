526f05a9150346b542cb24795277775a8eaaf60a
ShamitGupta <e1459301@u.nus.edu>
2026-03-25 17:41:33 +0800

    Added Shamo Logo

--- Chatbot/src/Sidebar/Sidebar.jsx
+++ Chatbot/src/Sidebar/Sidebar.jsx
@@ -1,8 +1,16 @@
 import styles from './Sidebar.module.css'
+import logo from '../assets/ShamoLogo.png'
 function Sidebar(){
     return(
         <div className = {styles.Body}>
+
             <div className = {styles.Sidebar}>
+
+                <div className = {styles.BotName}>
+                    <img src = {logo} className = {styles.Logo}></img>
+                    <p className = {styles.Label}>Shamo AI</p>  
+                </div>
+
                 <div className = {styles.SidebarButtonsContainer}>
                     <button className = {styles.SidebarButtons}>New Chat</button>
                     <button className = {styles.SidebarButtons}>About Us</button>
