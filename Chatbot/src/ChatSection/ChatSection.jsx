a787567fa9be8278a7c55768a8452d082ac59561
ShamitGupta <e1459301@u.nus.edu>
2026-03-26 10:49:26 +0800

    Added chatbot submit and necessary states

--- Chatbot/src/ChatSection/ChatSection.jsx
+++ Chatbot/src/ChatSection/ChatSection.jsx
@@ -1,10 +1,33 @@
 import styles from './ChatSection.module.css'
+import { useState } from 'react'
 
 function ChatSection(){
+
+    const [inputValue,setInputValue] = useState("");
+    const [prompt,setPrompt] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setInputValue("");
+        console.log(`Prompt is ${prompt}`);
+    }
+
     return(
         <div className = {styles.Body}>
             <div className = {styles.ChatSection}>
-                <input type = 'text' placeholder='Ask anything' className = {styles.ChatBox}></input>
+                <form onSubmit={handleSubmit} className = {styles.Form}>
+                    <input 
+                    type = 'text' 
+                    placeholder='Ask anything' 
+                    value = {inputValue} 
+                    onChange={(e) => {
+                        setInputValue(e.target.value);
+                        setPrompt(e.target.value);
+                    }} 
+                    className = {styles.ChatBox}>
+                    </input>
+                </form>
+                
             </div>
         </div>
     )
