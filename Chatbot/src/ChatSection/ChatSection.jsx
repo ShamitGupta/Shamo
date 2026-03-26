0d2742b6e4ea64cdc8126915af5e70be283e6c8e
ShamitGupta <e1459301@u.nus.edu>
2026-03-26 10:12:09 +0800

    Added Basic ChatBox with styling

--- Chatbot/src/ChatSection/ChatSection.jsx
+++ Chatbot/src/ChatSection/ChatSection.jsx
@@ -4,7 +4,7 @@ function ChatSection(){
     return(
         <div className = {styles.Body}>
             <div className = {styles.ChatSection}>
-
+                <input type = 'text' placeholder='Ask anything' className = {styles.ChatBox}></input>
             </div>
         </div>
     )
