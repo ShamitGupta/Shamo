c8c095248d22ccd89776506e05468d2eef0678d7
ShamitGupta <e1459301@u.nus.edu>
2026-04-06 12:58:00 +0800

    Integrated OpenAI API through env files with successful test output

--- Backend/openai_test.py
+++ Backend/openai_test.py
@@ -1,6 +1,12 @@
 from openai import OpenAI
 from dotenv import load_dotenv, dotenv_values
-client = OpenAI()
+import os
+
+
+load_dotenv()
+client = OpenAI(
+    api_key= os.getenv("OPENAI_API_KEY")
+)
 
 response = client.responses.create(
     model="gpt-5.4",
