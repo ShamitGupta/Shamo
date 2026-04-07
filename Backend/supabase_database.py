2bc4570bf4c2e516ed53660cf9d69db6060f8c15
ShamitGupta <e1459301@u.nus.edu>
2026-04-07 13:36:22 +0800

    Formatted the extracted data into a combined qp_data and ms_data variable

--- Backend/supabase_database.py
+++ Backend/supabase_database.py
@@ -24,17 +24,30 @@ def retrieve_info(metadata: InformationExtracter):
             .contains("metadata", search_criteria) \
             .execute()
         
-        print("Done")
         return response.data # Returns a list of matching documents
     except Exception as e:
         print(f"Error fetching from Supabase: {e}")
         return []
-
-test = information_extraction("Can you please help me figure out how to do question 7 of the May June 2024 P21?")
     
-output = retrieve_info(test)
-print(len(output))
-print(output)
+def format_data(retrieved_data: dict):
+    qp_data = ""
+    ms_data = ""
+
+    for i in range(len(retrieved_data)):
+        if(retrieved_data[i].get("metadata").get("Document_type") == "Question Paper"):
+            qp_data += retrieved_data[i].get("content")
+        
+        if(retrieved_data[i].get("metadata").get("Document_type") == "Mark Scheme"):
+            ms_data += retrieved_data[i].get("content")
+    
+
+
+# test = information_extraction("Can you please help me figure out how to do question 7 of the May June 2024 P21?")
+
+# output = retrieve_info(test)
+# format_data(output)
+
+
 
 
 
