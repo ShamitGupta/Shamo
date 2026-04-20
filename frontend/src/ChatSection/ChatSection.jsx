82d03106ed2c74e5d4218f0e3b9c16937fef0419
ShamitGupta <e1459301@u.nus.edu>
2026-04-20 20:46:33 +0800

    Fixed String issue for F/M and O/N papers

--- frontend/src/ChatSection/ChatSection.jsx
+++ frontend/src/ChatSection/ChatSection.jsx
@@ -176,9 +176,9 @@ function ChatSection() {
                     </select>
                     <select className={styles.SelectBtn} value={session} onChange={(e) => setSession(e.target.value)}>
                         <option value="">Session (None)</option>
-                        <option value="Feb/March">Feb/March</option>
+                        <option value="Feb/March">February/March</option>
                         <option value="May/June">May/June</option>
-                        <option value="Oct/Nov">Oct/Nov</option>
+                        <option value="Oct/Nov">October/November</option>
                     </select>
                     <select className={styles.SelectBtn} value={variant} onChange={(e) => setVariant(e.target.value)}>
                         <option value="">Variant (None)</option>
